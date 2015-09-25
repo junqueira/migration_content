76a0f04efc66c1849f7dbf15c96b495e4301b7dc
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2015-09-25 09:51:11 -0300

    test get migration target_metadata content.Base.metadata

---
+++ migration/versions/143fd932895c_create_migration_teste.py
@@ -0,0 +1,24 @@
+"""create migration teste
+
+Revision ID: 143fd932895c
+Revises: f4796b0a15e
+Create Date: 2015-09-24 17:17:05.612612
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '143fd932895c'
+down_revision = 'f4796b0a15e'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
